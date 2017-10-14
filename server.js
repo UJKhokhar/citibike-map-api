76f3041f9d89e48e45e74df3a8b7e564770a36b8
Umar Khokhar <ujkhokhar@gmail.com>
2017-10-13 23:05:58 -0400

    Setup post endpoint that sends trips

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
+import _ from 'lodash';
+import moment from 'moment';
+import tripdata from './tripdata/tripdata.json';
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+function getTrips(date, time) {
+  return _.filter(tripdata, trip => (
+    moment(trip.starttime).isSame(date, 'day') &&
+    moment(trip.starttime).isSameOrBefore(`${date} ${time}`) &&
+    moment(trip.stoptime).isSameOrAfter(`${date} ${time}`)
+  ));
+}
 
 app.listen(3030, () => {
   console.log('Started on port 3030');
 });
 
-app.get('/', (req, res) => {
-  console.log('Res', res);
-  res.send('hello world');
+app.post('/', (req, res) => {
+  const trips = getTrips(req.body.date, req.body.time);
+  console.log(trips);
+  res.send(trips);
 });
