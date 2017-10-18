2b4523bf5fcc47f35e7c6045c914b01af95e0732
Umar Khokhar <ujkhokhar@gmail.com>
2017-10-17 20:45:29 -0400

    Add cors. Properly format date

--- findTrip.js
+++ findTrip.js
@@ -3,15 +3,15 @@ const { MongoClient } = require('mongodb');
 const url = 'mongodb://localhost:27017/CitibikeTrips';
 
 export default function getTrips(date, time) {
+  const formattedDate = new Date(`${date}T${time}Z`);
   return MongoClient.connect(url)
     .then((db) => {
       return db.collection('trips').find({
-        starttime: { $lte: new Date(`${date} ${time}`) },
-        stoptime: { $gte: new Date(`${date} ${time}`) },
+        starttime: { $lte: formattedDate },
+        stoptime: { $gte: formattedDate },
       }).toArray()
     })
     .then((docs) => {
-      console.log('Docs', docs);
       return docs;
     })
     .catch((err) => {
--- server.js
+++ server.js
@@ -1,21 +1,28 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import getTrips from './findTrip';
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+const corsOptions = {
+  origin: 'http://localhost:8080',
+};
+
+app.options('*', cors());
+
 app.use(bodyParser.json());
+
 
 app.listen(3030, () => {
   console.log('Started on port 3030');
 });
 
-app.post('/', (req, res) => {
+app.post('/trips', cors(corsOptions), (req, res) => {
   getTrips(req.body.date, req.body.time)
     .then((trips) => {
-      console.log('Trips', trips);
       res.send(trips);
     })
     .catch((err) => {
