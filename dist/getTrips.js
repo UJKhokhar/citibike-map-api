8057b1fd7d136e01c011dba6500bcc6ca3a73744
Umar Khokhar <ujkhokhar@gmail.com>
2017-10-25 22:53:23 -0400

    Update mongo uri and remove map data script

--- dist/getTrips.js
+++ dist/getTrips.js
@@ -8,7 +8,7 @@ exports.default = getTrips;
 var _require = require('mongodb'),
     MongoClient = _require.MongoClient;
 
-var url = 'mongodb://localhost:27017/CitibikeTrips';
+var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/CitibikeTrips';
 
 function getTrips(date, time) {
   var formattedDate = new Date(date + 'T' + time + 'Z');
